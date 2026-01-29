5d1c7a076f9ea45f552057be1fff901be4794791
huaxiren6 <liushiyu20070115@outlook.com>
2026-01-29 18:00:08 +0800

    1
    1

--- Project/code/LED.c
+++ Project/code/LED.c
@@ -1,17 +1,17 @@
-#include "LED.h"
-
-void LED_Init(void)
-{
-    // 初始化为推挽输出，默认高电平(灭)
-    gpio_init(LED_PIN, GPO, 1, GPO_PUSH_PULL);
-}
-
-void LED_ON(void)
-{
-    gpio_set_level(LED_PIN, 0); // 低电平亮
-}
-
-void LED_OFF(void)
-{
-    gpio_set_level(LED_PIN, 1); // 高电平灭
-}
+#include "LED.h"
+
+void LED_Init(void)
+{
+    // 初始化为推挽输出，默认高电平(灭)
+    gpio_init(LED_PIN, GPO, 1, GPO_PUSH_PULL);
+}
+
+void LED_ON(void)
+{
+    gpio_set_level(LED_PIN, 0); // 低电平亮
+}
+
+void LED_OFF(void)
+{
+    gpio_set_level(LED_PIN, 1); // 高电平灭
+}
